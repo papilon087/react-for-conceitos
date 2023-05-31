45fc4e143623e1e7ad7ad9551be9bfdb61c04bde
papilon <ademirgus@gmail.com>
2023-05-30 21:00:29 -0300

    feat:add dashboard and react router dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import // BrowserRouter,
+// Route,
+// Routes as Switch,
+// Navigate,
+"react-router-dom";
+
 import reportWebVitals from "./reportWebVitals";
-import { App } from "./App";
+import { App } from "./app/App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
