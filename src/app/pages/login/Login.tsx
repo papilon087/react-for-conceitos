c769408d315164cf11ced7ae949cc05f05523bb3
papilon <ademirgus@gmail.com>
2023-06-01 20:56:02 -0300

    feat:add login page and form

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,16 +1,38 @@
-import { useNavigate } from "react-router-dom";
-
 // Dentro de uma função useNavigate
-export const Login = () => {
-  const history = useNavigate();
+// Iniciamos components com valores vazios.
 
-  const handleClick = () => {
-    history("/pagina-inicial");
+import { useState } from "react";
+
+export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEntrar = () => {
+    console.log(email);
+    console.log(password);
   };
+
   return (
     <div>
-      Login
-      <button onClick={handleClick}>Página inicial</button>
+      <form>
+        <label>
+          <span>Email</span>
+          <input value={email} onChange={(e) => setEmail(e.target.value)} />
+        </label>
+
+        <label>
+          <span>Senha</span>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+
+        <button type="button" onClick={handleEntrar}>
+          Entrar
+        </button>
+      </form>
     </div>
   );
 };
