3f50093b5fd21f3ee94a1c4b2a03fd07d4d0ca09
papilon <ademirgus@gmail.com>
2023-06-15 20:39:53 -0300

    feat:useCallback function

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,17 +1,19 @@
-import { useMemo, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 
 export const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  //Guarda o valor em memória e contabiliza.
   const emailLength = useMemo(() => {
     return email.length * 1000;
   }, [email.length]);
 
-  const handleEntrar = () => {
+  //Guarda o valor da função, não reconstruindo a função toda vez que o array de dependencias não for informado.
+  const handleEntrar = useCallback(() => {
     console.log(email);
     console.log(password);
-  };
+  }, [email, password]);
 
   return (
     <div>
